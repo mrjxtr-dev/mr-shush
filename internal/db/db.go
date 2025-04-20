7b9e8c94a7447ffe940a6ef8141d158663cf24a8
Jester Lumacad <mrjxtr.dev@gmail.com>
2025-04-20 21:38:13 +0800

    feat: added sqlite3 database

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,17 @@
 package db
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB(dbSource string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dbSource)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	return db, nil
+}
